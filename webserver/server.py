cf5c941cc6a092ed28fbf472a907f887b6d59e2d
mfabros <melissa.fabros@gmail.com>
2015-05-15 00:10:49 -0700

    improved code for webserver exercises

--- webserver/server.py
+++ webserver/server.py
@@ -1,5 +1,5 @@
-import socket
-import httplib 
+import socket, urllib2
+
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.bind(('', 5000))
@@ -9,29 +9,35 @@ server_sock.listen(10)
 while True:
     client_sock, addr = server_sock.accept()
     print 'We have opened a socket!'
-    headers = client_sock.recv(100)  #  are the incoming headers
-    headers = str(headers)
+    headers = client_sock.recv(100)  #  are the incoming headers, saved into variable
+    headers = str(headers) #turn headers in string
     print headers
      
-    split_headers = headers.split('\n')
-    first_line = split_headers[0]
-    parse_line1 = first_line.split()
-    path = parse_line1[1]
+    split_headers = headers.split('\n') #split headers at newline \n
+    first_line = split_headers[0] #store GET header into first_line
+    parse_line1 = first_line.split() #split first line to isolate path and store as variable
+    path = parse_line1[1] #store path
+    print path
     
-     if len(path) == 1:
-        with open('kittens.html') as f:
-            
-    
-    if len(path) > 1:
-        with open('error.html') as e:
-            client_sock.send(e)
-    else:
-        client_sock.send("I'm a 404 error")
+    if len(path) == 1: #if no path is sent from client to server open kittens.html
+        with open('kittens.html') as f: #opens kittens.html
+           htmlstring_kittens = str(f.read()) #opens error.html as string saves in variable
+           print htmlstring_kittens
+        client_sock.send(htmlstring_kittens) #opens html file as text file of html markdown
+          
+    if len(path) > 1: #if any path is sent from client to server open error.html
+        with open('error.html')as e: #opens error.html
+            htmlstring_error = str(e.read())  #opens html file as text file of html markdown, saves in variable
+            print htmlstring_error
+        client_sock.send(htmlstring_error)  #sends text file to client
+        
+
+        if path != "/" :
+            error_msg = "<h1> Ooops, I did it again.\n Error 418. It's Teatime</h1>"
+            client_sock.send(error_msg)
             
 
-        
     output = "<h1>Hello Client</h1>"
-#    client_sock.send("HTTP/1.1 200 OK\n" + "Content length: "+str(len(output)) + "Content-Type: text/html\n\n" + output)
-    client_sock.send("HTTP/1.1 418 It's Teatime\n" + "Content length: "+str(len(output)) + "Content-Type: text/html\n\n" + output)    
+    client_sock.send("HTTP/1.1 200 OK\n" + "Content length: "+str(len(output)) + "Content-Type: text/html\n\n" + output)
     
     client_sock.close()
